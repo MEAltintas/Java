2f99ff898fc137859f8b3f27886cbc63b64247fd
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-02-05 15:36:11 +0300

    Add files via upload
    Javada değişkenleri tanımlama örneğidir

---
+++ Degiskenler.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Degiskenler {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayi1 = 5;
+        String cumle = "Merhaba, nasılsın?";
+        double sayi2 = 3.14;
+        char harf = 'A';
+        System.out.println(sayi1);
+        System.out.println(sayi2);
+        System.out.println(cumle);
+        System.out.println(harf);
+
+    }
+
+}
