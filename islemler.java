a4b0a3553d61a6679bf791853296ccfc653c3e31
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-02-06 22:46:16 +0300

    İşlemler
    double olarak tanımlanan sayılar ile yazdırma işlemi esnasında dört işlem yapar.

---
+++ islemler.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class islemler {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = 7;
+        double b = 5;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+        System.out.println(a == b);
+    }
+
+}
