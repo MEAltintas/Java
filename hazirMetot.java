4059ff57581468e94327d085ec77381948e45972
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-02-07 23:49:29 +0300

    Java kütüphanesindeki bazı hazır metotlar

---
+++ hazirMetot.java
@@ -0,0 +1,13 @@
+
+public class hazirMetot {
+
+    public static void main(String[] args) {
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(7, 1));
+        System.out.println(Math.sqrt(81));
+        System.out.println(Math.round(48.345));
+        System.out.println(Math.abs(-4.7));
+        System.out.println(Math.random());
+    }
+
+}
