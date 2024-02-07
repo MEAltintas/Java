f7143b3a7ffd2ac75712815855d937cf1bc1c9fd
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-02-07 23:48:13 +0300

    While örneği

---
+++ whileOrnek.java
@@ -0,0 +1,15 @@
+
+public class whileOrnek {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println("Ben bir ceviz ağacıyım gülhane parkında");
+            i += 3;
+        }
+
+        System.out.println("i'nin değeri kaç: " + i);
+        
+    }
+
+}
