3cc26dff6ac90293cfaddb4be17ab8a7d8baaa62
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-02-09 23:10:58 +0300

    41 Kere Maşallah

---
+++ kırkBirKereMasallah.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class kırkBirKereMasallah {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 1; i <= 41; i++) {
+            System.out.println(i + " kere Maşallah");
+
+        }
+    }
+
+}
