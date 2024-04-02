7b4bb3e1fecd2bed885348a048de6ae32dfa098c
Münüre Ezgi Altıntaş <101413474+MEAltintas@users.noreply.github.com>
2024-04-02 21:46:50 +0300

    Matris İzi
    Matris izini hesaplar.

---
+++ matrisİzi.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class matrisİzi {
+
+    public static void main(String[] args) {
+        System.out.print("Kare matrisin tipini giriniz: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int A[][] = new int[n][n];
+        int T = 0;
+        System.out.println("A matrisi: ");
+        System.out.println("-------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("A[" + i + "," + j + " ]=?");
+                A[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("girilen matris: ");
+        System.out.println("-------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(A[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            T += A[i][i];
+        }
+        System.out.println("Matrisin izi: " + T);
+    }
+
+}
